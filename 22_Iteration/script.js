4b1de28a1fc64ee56435474685ca8c37b7a31e5b
Prem Lalwani <premprakashlalwani@gmail.com>
2024-06-15 19:19:17 +0530

    Loops in JS Added

---
+++ 22_Iteration/script.js
@@ -0,0 +1,67 @@
+// FOR LOOP
+
+// for (let i = 0; i < 10 ; i++) {
+    
+//     if (i == 5) {
+//         // break;
+//         continue;
+        
+//     }
+//     console.log(i);
+    
+// }
+
+
+
+
+
+// for (let i = 0; i < 3; i++) {
+//     console.log(`Outer Loop value : ${i}`);
+//     for (let j = 0; j < 3; j++) {
+//         console.log(`Inner Loop value ${i} and inner loop ${j}`);
+        
+//     }   
+// }
+
+
+
+
+// let myArray = ["Flash", "Batman", "Superman"]
+
+// for (let index = 0; index < myArray.length; index++) {
+//     const element = myArray[index];
+//     console.log(element); 
+// }
+
+
+
+
+// WHILE LOOP
+// let i=0;
+// while (i < 10) {
+//     console.log(i);
+//     i += 2;
+// }
+
+
+
+
+
+
+// let myArray = ["Flash", "Batman", "Superman"]
+// let i=0;
+// while (i < myArray.length) {
+//     console.log(myArray[i]);
+//     i++;
+// }
+
+
+
+
+
+// DO-WHILE LOOP
+let score = 1
+do{
+    console.log(score);
+    score++;
+} while(score <= 10);
